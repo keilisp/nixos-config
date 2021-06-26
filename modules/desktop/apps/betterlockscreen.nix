a623efee264f558df679dc83d637a33038b79d4a
Druk Oleksandr <druksasha@ukr.net>
2021-06-26 21:25:26 +0300

    betterlockscreen on sleep/suspend

--- modules/desktop/apps/betterlockscreen.nix
+++ modules/desktop/apps/betterlockscreen.nix
@@ -6,5 +6,28 @@ let cfg = config.modules.desktop.apps.betterlockscreen;
 in {
   options.modules.desktop.apps.betterlockscreen = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { user.packages = with pkgs; [ betterlockscreen ]; };
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ betterlockscreen ];
+
+    systemd.services.betterlockscreen = {
+      enable = true;
+      # unitConfig = {
+      description = "Lock screen when going to sleep/suspend";
+      before = [ "sleep.target" "suspend.target" ];
+      # Before = "suspend.target";
+      # };
+
+      wantedBy = [ "sleep.target" "suspend.target" ];
+
+      serviceConfig = {
+        User = "kei";
+        Type = "forking";
+        Environment = "DISPLAY=:0";
+        ExecStart = "${pkgs.betterlockscreen}/bin/betterlockscreen --lock";
+        TimeoutSec = "infinity";
+        # ExecStartPost = ''${pkgs.sleep}/bin/sleep 1'';
+      };
+    };
+  };
 }
+
