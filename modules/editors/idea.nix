cbfb7f68d99b89586f9cb495bd4d056bb708e6b1
Druk Oleksandr <druksasha@ukr.net>
2021-09-11 19:25:00 +0300

    +idea (u need to know your enemy)

---
+++ modules/editors/idea.nix
@@ -0,0 +1,13 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.editors.idea;
+    configDir = config.dotfiles.configDir;
+in {
+  options.modules.editors.idea = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ jetbrains.idea-community ];
+  };
+}
