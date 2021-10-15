0cd12b02ee13bdd6fdaa4389090e7f2ee4d81bbe
Druk Oleksandr <druksasha@ukr.net>
2021-10-15 22:16:31 +0300

    attempt to fix teamviewer pid issue

--- modules/services/teamviewer.nix
+++ modules/services/teamviewer.nix
@@ -4,11 +4,18 @@ with lib;
 with lib.my;
 let cfg = config.modules.services.teamviewer;
 in {
-  options.modules.services.teamviewer = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.services.teamviewer = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     services.teamviewer.enable = true;
+    # systemd.services.teamviewerd.serviceConfig = lib.mkForce {
+    #   Type = "forking";
+    #   ExecStart = "${pkgs.teamviewer}/bin/teamviewerd daemon start";
+    #   GuessMainPID = true;
+    #   ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+    #   Restart = "on-abort";
+    #   StartLimitInterval = "60";
+    #   StartLimitBurst = "10";
+    # };
   };
 }
