c43349487f62773200191d82f7234a42a3829bb3
Druk Oleksandr <druksasha@ukr.net>
2022-04-21 21:41:03 +0300

    flakes bump

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = 
     {
       # Core dependencies.
-      # nixpkgs.url = "nixpkgs/nixos-unstable";             # primary nixpkgs
-      nixpkgs.url = "nixpkgs/nixos-21.11";             # primary nixpkgs
+      nixpkgs.url = "nixpkgs/nixos-unstable";             # primary nixpkgs
+      # nixpkgs.url = "nixpkgs/nixos-21.11";             # primary nixpkgs
       nixpkgs-unstable.url = "nixpkgs/nixos-unstable";  # for packages on the edge
       home-manager.url = "github:rycee/home-manager/master";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
