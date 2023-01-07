92316b926b890622b0c17528fdd28176b7b84c11
Druk Oleksandr <druksasha@ukr.net>
2023-01-07 22:41:05 +0200

    Migrate to 'modus=theme' 4.0.0 version

--- modules/themes/modus-operandi/config/emacs/current-theme.el
+++ modules/themes/modus-operandi/config/emacs/current-theme.el
@@ -5,18 +5,41 @@
 (use-package modus-themes
   :ensure t
   :custom
-  ;; (modus-themes-syntax '(faint yellow-comments alt-syntax))
-  ;; (modus-themes-deuteranopia t)
-  ;; (modus-themes-completions 'moderate)
-  (modus-themes-region '(no-extend bg-only))
+  (modus-themes-completions nil)
+  (modus-themes-variable-pitch-ui t)
+  (modus-themes-mixed-fonts t)
   (modus-themes-org-blocks 'gray-background)
-  (modus-themes-paren-match '(bold))
-  (modus-themes-hl-line '(accented))
   (modus-themes-italic-constructs t)
-  (modus-themes-lang-checkers '(background text-also))
-  (modus-themes-links '(no-underline background))
-  (modus-themes-mode-line '(borderless 4))
   (modus-themes-prompts '(bold))
+  (modus-themes-headings '((1 . (light variable-pitch 1.1))
+                           (agenda-date . (1.1))
+                           (agenda-structure . (variable-pitch light 1.2))
+                           (t . (medium))))
+  (modus-themes-common-palette-overrides 
+   `(
+     ;; Make the mode-line borderless and stand out less
+     (bg-mode-line-active bg-inactive)
+     (fg-mode-line-active fg-main)
+     (bg-mode-line-inactive bg-inactive)
+     (fg-mode-line-active fg-dim)
+
+     (border-mode-line-active bg-main)
+     (border-mode-line-inactive bg-inactive)
+
+     ;; Globally use faint instead of warm blue and disable underlines for links
+     (fg-link blue-faint)
+     (underline-link unspecified)
+     (underline-link-visited unspecified)
+     (underline-link-symbolic unspecified)
+     
+     ;; Enable underlines for parens
+     ;; (bg-paren-match bg-magenta-intense) ; set highlight color to magenta
+     (underline-paren-match fg-main)
+   
+     ;; Make linters underlines less intense
+     (underline-err red-faint)
+     (underline-warning yellow-faint)
+     (underline-note cyan-faint)))
   :init
   ;; Terminal mode
   (unless (display-graphic-p)
