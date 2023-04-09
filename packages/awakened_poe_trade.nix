693bc53eddeab7a94d9951aca9183101a0abf193
Druk Oleksandr <druksasha@ukr.net>
2023-04-09 20:08:15 +0300

    Update 'awakened-poe-trade' version for Crucible league

--- packages/awakened_poe_trade.nix
+++ packages/awakened_poe_trade.nix
@@ -2,13 +2,14 @@
 
 let
   name = "Awakened-PoE-Trade";
-  version = "3.19.10002";
+  version = "3.20.10008";
 in pkgs.appimageTools.wrapType2 {
   inherit name version;
   
+  # TODO check out --no-overlay flag
   src = pkgs.fetchurl {
     url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v${version}/${name}-${version}.AppImage";
-    sha256 = "/ojEoH0dEM14GvxnS8KLYAYRLbZMkP8smRQZdtTnSrk=";
+    sha256 = "05XSA+7CymwvRTGGeBO2m+CGGIYqapzY9o9u2DzQGiY=";
 
   };
 }
